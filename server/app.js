6ea304ea1c2be59d2a8a03814228e472e09f1416
Matias Zanolli <z_killemall@yahoo.com>
2014-02-20 20:23:37 -0200

    Estructura basica en node/express

---
+++ server/app.js
@@ -0,0 +1,39 @@
+/*var server = require("./server");
+
+var handlers = {}
+	handlers["/"] = requestHandlers.iniciar;
+	handlers["/iniciar"] = requestHandlers.iniciar;
+
+server.iniciar();
+*/
+
+// Include the Node HTTP library
+var http = require('http');
+// Include the Express module
+var express = require('express');
+var cons = require('consolidate');
+// Create an instance of Express
+var app = express();
+
+console.log('Starting app...');
+
+app.configure(function() {
+	// assign the underscore engine to .html files
+	app.engine('html', cons.underscore);
+
+	// set .html as the default extension 
+	app.set('view engine', 'html');
+	app.set('views', __dirname + '/views');
+	app.use(express.static(__dirname + 'static'));
+});
+
+// Start the app
+http.createServer(app).listen(3000, function() {
+  console.log('Express app started');
+});
+// A route for the home page
+app.get('/', function(req, res) {
+  res.render('index', {
+    title: 'Consolidate.js'
+  });
+});
--- server/index.js
+++ server/index.js
@@ -1,34 +1,10 @@
-/*var server = require("./server");
+define(['text!templates/index.html'], function(indexTemplate) {
+	var indexView = Backbone.View.extend({
+		el: $('#content'),
+		render: function() {
+			this.$el.html(indexTemplate);
+		}
+	});
 
-var handlers = {}
-	handlers["/"] = requestHandlers.iniciar;
-	handlers["/iniciar"] = requestHandlers.iniciar;
-
-server.iniciar();
-*/
-
-// Include the Node HTTP library
-var http = require('http');
-// Include the Express module
-var express = require('express');
-var cons = require('consolidate');
-// Create an instance of Express
-var app = express();
-
-// assign the underscore engine to .html files
-app.engine('html', cons.underscore);
-
-// set .html as the default extension 
-app.set('view engine', 'html');
-app.set('views', __dirname + '/views');
-
-// Start the app
-http.createServer(app).listen(3000, function() {
-  console.log('Express app started');
-});
-// A route for the home page
-app.get('/', function(req, res) {
-  res.render('index', {
-    title: 'Consolidate.js'
-  });
+	return new indexView;
 });
